8cda045d2b85ec54e4af39ef1fac2fdb6874044e
Ori5000 <43366498+Ori5000@users.noreply.github.com>
2022-07-15 14:18:27 +0300

    Update OmnyDLColab.py

--- OmnyDLColab.py
+++ OmnyDLColab.py
@@ -1,31 +1,49 @@
 import re
 import os
 import wget
+import argparse
+
+arguments = argparse.ArgumentParser()
+arguments.add_argument("-m", '--manual', dest="manRSS", help="Choose a custom RSS URL manually.")
+args = arguments.parse_args()
+
+if args.manRSS:
+    manRSS = str(args.manRSS)
 
 if not os.path.exists('Omny-Downloader/config.ini'):
 	open('Omny-Downloader/config.ini', 'a').close()
 
 if os.stat('Omny-Downloader/config.ini').st_size==0:
 	config = open('Omny-Downloader/config.ini', 'w+')
-	showName = input('Please insert the omny.fm show name you want to download: ')
 	downPath = input('Please insert the location you want the podcasts to be downloaded to (Default is /content/drive/MyDrive/Omny Podcasts/, click ENTER to use that.)')
 	if downPath == '':
 		downPath = '/content/drive/MyDrive/Omny Podcasts/'
-	config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}\nDownPath = {downPath}')
+	if not args.manRSS:
+		showName = input('Please insert the omny.fm show name you want to download: ')
+		config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}\nDownPath = {downPath}')
+	else:
+		config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nDownPath = {downPath}')
 else:
 	config = open('Omny-Downloader/config.ini', 'r+')
 	try:
-		showName = config.readlines()[1].rstrip()[11:]
+		if not args.manRSS:
+			showName = config.readlines()[1].rstrip()[11:]
 		downPath = config.readlines()[2].rstrip()[11:]
 	except:
 		config = open('Omny-Downloader/config.ini', 'w+')
-		showName = input('Please insert the omny.fm show name you want to download: ')
 		downPath = input('Please insert the location you want the podcasts to be downloaded to (Default is /content/drive/MyDrive/Omny Podcasts/, click ENTER to use that.)')
 		if downPath == '':
 			downPath = '/content/drive/MyDrive/Omny Podcasts/'
+	if not args.manRSS:
+		showName = input('Please insert the omny.fm show name you want to download: ')
 		config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}\nDownPath = {downPath}')
+	else:
+		config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nDownPath = {downPath}')
 
-RSS_URL = f'https://omny.fm/shows/{showName}/playlists/podcast.rss'
+if args.manRSS:
+	RSS_URL = manRSS
+else:
+	RSS_URL = f'https://omny.fm/shows/{showName}/playlists/podcast.rss'
 URL_REGEX = r"""((?:(?:https|ftp|http)?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|org|uk)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|uk|ac)\b/?(?!@)))"""
 
 if os.path.exists('Omny-Downloader/podcast.rss'):
@@ -44,10 +62,10 @@ with open("Omny-Downloader/podcast.rss",encoding ="utf8") as f:
    rss = f.read()
 
 urls = re.findall(URL_REGEX, rss)
-VALID_DOMAIN = 'traffic.omny.fm/d/clips/'
+VALID_DOMAINS = ['mp3', 'm4a']
 valid_urls = []
 for url in urls:
-	if VALID_DOMAIN in url:
+	if any(domain in url for domain in VALID_DOMAINS):
 		if not url in valid_urls:
 			valid_urls.append(url)
 
@@ -60,9 +78,11 @@ with open("Omny-Downloader/podcast.rss", "r", encoding="utf8") as rss:
 			title = title[7:-8]
 			title = title.replace('|', '-')
 			titles.append(title)
+showName = titles[0]
 titles = titles[2:]
 
 os.makedirs(f'{downPath}{showName}', exist_ok=True)
+invalidChar = '<>:"/\|?*'
 
 if os.path.exists(f'{downPath}{showName}/log.txt'):
 	with open(f'{downPath}{showName}/log.txt', 'r+') as f:
@@ -76,6 +96,8 @@ if os.path.exists(f'{downPath}{showName}/log.txt'):
 				f.write(" ".join(['Title:', titles_i, 'URL:', valid_urls_i]))
 				f.write("\n")
 				print(f'\nDownloading {titles_i}')
+				for char in invalidChar:
+					titles_i = titles_i.replace(char, '')
 				wget.download(valid_urls_i, out=f'{downPath}{showName}/{titles_i}.mp3')
 else:
 	with open(f'{downPath}{showName}/log.txt', 'w+') as f:
@@ -89,4 +111,6 @@ else:
 				f.write(" ".join(['Title:', titles_i, 'URL:', valid_urls_i]))
 				f.write("\n")
 				print(f'\nDownloading {titles_i}')
+				for char in invalidChar:
+					titles_i = titles_i.replace(char, '')
 				wget.download(valid_urls_i, out=f'{downPath}{showName}/{titles_i}.mp3')
