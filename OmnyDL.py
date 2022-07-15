922200e5a6c7e7cd131e7bcbd97359bbd781828a
Ori5000 <43366498+Ori5000@users.noreply.github.com>
2022-07-15 14:00:55 +0300

    Added option to download podcasts from any site

--- OmnyDL.py
+++ OmnyDL.py
@@ -1,24 +1,36 @@
 import re
 import os
 import wget
+import argparse
 
-if not os.path.exists('config.ini'):
-	open('config.ini', 'a').close()
+arguments = argparse.ArgumentParser()
+arguments.add_argument("-m", '--manual', dest="manRSS", help="Choose a custom RSS URL manually.")
+args = arguments.parse_args()
 
-if os.stat('config.ini').st_size==0:
-	config = open('config.ini', 'w+')
-	showName = input('Please insert the omny.fm show name you want to download: ')
-	config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}')
-else:
-	config = open('config.ini', 'r+')
-	try:
-		showName = config.readlines()[1].rstrip()[11:]
-	except:
+if args.manRSS:
+    manRSS = str(args.manRSS)
+
+if not args.manRSS:
+	if not os.path.exists('config.ini'):
+		open('config.ini', 'a').close()
+
+	if os.stat('config.ini').st_size==0:
 		config = open('config.ini', 'w+')
 		showName = input('Please insert the omny.fm show name you want to download: ')
 		config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}')
+	else:
+		config = open('config.ini', 'r+')
+		try:
+			showName = config.readlines()[1].rstrip()[11:]
+		except:
+			config = open('config.ini', 'w+')
+			showName = input('Please insert the omny.fm show name you want to download: ')
+			config.write(f'#---===--- Omny.fm Podcast Downloader By OREEE ---===---#\nShowName = {showName}')
 
-RSS_URL = f'https://omny.fm/shows/{showName}/playlists/podcast.rss'
+if args.manRSS:
+	RSS_URL = manRSS
+else:
+	RSS_URL = f'https://omny.fm/shows/{showName}/playlists/podcast.rss'
 URL_REGEX = r"""((?:(?:https|ftp|http)?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|org|uk)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|uk|ac)\b/?(?!@)))"""
 
 if os.path.exists('podcast.rss'):
@@ -37,10 +49,10 @@ with open("podcast.rss",encoding ="utf8") as f:
    rss = f.read()
 
 urls = re.findall(URL_REGEX, rss)
-VALID_DOMAIN = 'traffic.omny.fm/d/clips/'
+VALID_DOMAINS = ['mp3', 'm4a']
 valid_urls = []
 for url in urls:
-	if VALID_DOMAIN in url:
+	if any(domain in url for domain in VALID_DOMAINS):
 		if not url in valid_urls:
 			valid_urls.append(url)
 
@@ -53,9 +65,11 @@ with open("podcast.rss", "r", encoding="utf8") as rss:
 			title = title[7:-8]
 			title = title.replace('|', '-')
 			titles.append(title)
+showName = titles[0]
 titles = titles[2:]
 
 os.makedirs(showName, exist_ok=True)
+invalidChar = '<>:"/\|?*'
 
 if os.path.exists(f'{showName}\log.txt'):
 	with open(f'{showName}\log.txt', 'r+') as f:
@@ -69,6 +83,8 @@ if os.path.exists(f'{showName}\log.txt'):
 				f.write(" ".join(['Title:', titles_i, 'URL:', valid_urls_i]))
 				f.write("\n")
 				print(f'\nDownloading {titles_i}')
+				for char in invalidChar:
+					titles_i = titles_i.replace(char, '')
 				wget.download(valid_urls_i, out=f'{showName}\{titles_i}.mp3')
 else:
 	with open(f'{showName}\log.txt', 'w+') as f:
@@ -82,4 +98,6 @@ else:
 				f.write(" ".join(['Title:', titles_i, 'URL:', valid_urls_i]))
 				f.write("\n")
 				print(f'\nDownloading {titles_i}')
+				for char in invalidChar:
+					titles_i = titles_i.replace(char, '')
 				wget.download(valid_urls_i, out=f'{showName}\{titles_i}.mp3')
